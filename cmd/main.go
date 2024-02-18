82a1c72b041bff293103713f1f507468f3be562b
navn8 <80399317+navn8@users.noreply.github.com>
2024-02-19 01:22:01 +0530

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -7,28 +7,28 @@ import (
 
 func main() {
 	fmt.Println("########### Empty ###########")
-	filePath := "/Users/trip/Work/mf/hello/log-analyser/log_empty.txt"
+	filePath := "log-analyser/log_empty.txt"
 	reader.Parse(filePath)
 	findTop := 10
 	result := reader.RankByFrequency(reader.AvgProcessingTime, findTop)
 	fmt.Printf("%v", result)
 
 	fmt.Println("\n \n ############# Normal ##########")
-	filePath = "/Users/trip/Work/mf/hello/log-analyser/log.txt"
+	filePath = "log-analyser/log.txt"
 	reader.Parse(filePath)
 	findTop = 10
 	result = reader.RankByFrequency(reader.AvgProcessingTime, findTop)
 	fmt.Printf("%v", result)
 
 	fmt.Println("\n \n ############# All Error Status != 200 ##########")
-	filePath = "/Users/trip/Work/mf/hello/log-analyser/log_error.txt"
+	filePath = "log-analyser/log_error.txt"
 	reader.Parse(filePath)
 	findTop = 10
 	result = reader.RankByFrequency(reader.AvgProcessingTime, findTop)
 	fmt.Printf("%v", result)
 
 	fmt.Println("\n \n ############# Ending with Gif ##########")
-	filePath = "/Users/trip/Work/mf/hello/log-analyser/log_gif.txt"
+	filePath = "log-analyser/log_gif.txt"
 	reader.Parse(filePath)
 	findTop = 10
 	result = reader.RankByFrequency(reader.AvgProcessingTime, findTop)
